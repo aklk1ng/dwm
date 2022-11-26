fb9c6a8fbfa5deba938f0814563c6d2f4ddfae9a
aklk1ng <2628084745@qq.com>
2022-11-26 19:53:06 +0800

    change the terminal from st to alacritty

--- config.h
+++ config.h
@@ -120,7 +120,8 @@ static Key keys[] = {
     { MODKEY,                   XK_F7,                  spawn,            {.v = (const char*[]){ "mpc", "pause", NULL }} },   /*暂停播放*/
     { MODKEY,                   XK_F8,                  spawn,            {.v = (const char*[]){ "mpc", "toggle", NULL }} },   /*继续播放*/ 
     { MODKEY,                   XK_F9,                  spawn,            {.v = (const char*[]){ "mpc", "stop", NULL }} },   /*停止播放*/
-    { MODKEY,              XK_Return, spawn, SHCMD("st") },                                                     /* super enter      | 打开st终端             */
+    /* { MODKEY,              XK_Return, spawn, SHCMD("st") },                                                     /1* super enter      | 打开st终端             *1/ */
+    { MODKEY,              XK_Return, spawn, SHCMD("alacritty") },                                                     /* super enter      | 打开alacritty终端             */
     { MODKEY,              XK_minus,  spawn, SHCMD("floatst") },                                                /* super +          | 打开浮动st终端         */
     { MODKEY,              XK_d,      spawn, SHCMD("rofi -theme nord -show drun -show-icons") },                /* super d          | rofi: 执行命令         */
     { MODKEY,              XK_p,      spawn, SHCMD("rofi -show menu -modi 'menu:~/scripts/rofi.sh'") },        /* super p          | rofi: 执行命令         */
