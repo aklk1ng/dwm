b5b3ff061917c9879db08ce4adab7ba5f55e730e
aklk1ng <2628084745@qq.com>
2023-06-15 21:17:27 +0800

    update date.sh

--- statusbar/packages/date.sh
+++ statusbar/packages/date.sh
@@ -2,28 +2,18 @@
 # DATE 获取日期和时间的脚本
 
 notify() {
-    _cal=$(cal --color=always | sed 's/..7m/<b><span color="#ff79c6">/;s/..27m/<\/span><\/b>/' )
-    # _todo=$(cat ~/.todo.md | sed 's/\(- \[x\] \)\(.*\)/<span color="#ff79c6">\1<s>\2<\/s><\/span>/' | sed 's/- \[[ |x]\] //')
-    # notify-send "  Calendar" "\n$_cal\n————————————————————\n$_todo" -r 9527
+    # _cal=$(cal --color=always | sed 's/..7m/<b><span color="#ff79c6">/;s/..27m/<\/span><\/b>/' )
+    _cal=$(cal --color=always)
     notify-send "  Calendar" "\n$_cal\n————————————————————" -r 9527
-}
-
-call_todo() {
-    pid1=`ps aux | grep 'st -t statusutil' | grep -v grep | awk '{print $2}'`
-    pid2=`ps aux | grep 'st -t statusutil_todo' | grep -v grep | awk '{print $2}'`
-    mx=`xdotool getmouselocation --shell | grep X= | sed 's/X=//'`
-    my=`xdotool getmouselocation --shell | grep Y= | sed 's/Y=//'`
-    kill $pid1 && kill $pid2 || st -t statusutil_todo -g 50x15+$((mx - 200))+$((my + 20)) -c FGN -e nvim ~/.todo.md
 }
 
 click() {
     case "$1" in
-        L) notify ;;
-        # R) call_todo ;;
+    L) notify ;;
     esac
 }
 
 case "$1" in
-    click) click $2 ;;
-    notify) notify ;;
+click) click $2 ;;
+notify) notify ;;
 esac
